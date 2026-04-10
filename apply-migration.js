d1ece710302fce54fe3690f73c2bdf83a52ad5d9
Naim <naimekamel@gmail.com>
2026-04-10 15:48:39 -0400

    chore: remove temp debug and backfill scripts

--- apply-migration.js
+++
@@ -1,6 +0,0 @@
-const{PrismaClient}=require('@prisma/client');
-const p=new PrismaClient();
-p.$queryRawUnsafe('ALTER TABLE "daily_fees" ADD COLUMN IF NOT EXISTS "awdStorageFee" DECIMAL(14,4) NOT NULL DEFAULT 0')
-.then(()=>console.log('column added'))
-.catch(e=>console.error(e))
-.finally(()=>p.$disconnect());
--- check-cursor.js
+++
@@ -1,6 +0,0 @@
-const{PrismaClient}=require('@prisma/client');
-const p=new PrismaClient();
-p.$queryRawUnsafe("SELECT \"jobName\", cursor FROM sync_cursors WHERE \"jobName\" = 'sync-settlement-refunds'")
-.then(r=>console.log(r))
-.catch(e=>console.error(e))
-.finally(()=>p.$disconnect());
